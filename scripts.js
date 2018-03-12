abb835b1d668ccf8dd361e6349155ca822dd0a5e
Perry <brothergilburt@gmail.com>
2018-03-12 18:00:29 -0500

    Implemented Menu Icon
    Implemented the menu icon in the HTML and CSS, then added Javascript to
    properly toggle the menu icon and remove/reset as the screen width
    changes.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
 window.addEventListener('DOMContentLoaded', function() {
   let headerMenu = document.getElementById('header-navigation-menu')
-  let headerMenuIcon = document.getElementById('header-navigation-menu-icon')
+  let headerMenuLink = document.getElementById('header-navigation-menu-link')
+  let headerMenuLabel = document.getElementById('header-menu-label')
 
   /*
    * Toggle header navigation menu visibility.
    */
-  headerMenuIcon.addEventListener('click', function() {
-    if (headerMenu.style.display === 'block') headerMenu.style.display = 'none'
-    else headerMenu.style.display = 'block'
+  headerMenuLink.addEventListener('click', function() {
+    if (headerMenu.style.display === 'block') {
+      headerMenu.style.display = 'none'
+      headerMenuLabel.innerHTML = 'Open'
+    } else {
+      headerMenu.style.display = 'block'
+      headerMenuLabel.innerHTML = 'Close'
+    }
   })
+
+  let desktop = window.matchMedia('(max-width: 400px)')
+  desktop.onchange = function(e) {
+    if (e.matches) {
+      //Set menu for mobile screen sizes.
+      headerMenuLabel.innerHTML = 'Open'
+      headerMenu.style.display = 'none'
+      headerMenuLink.style.display = 'flex';
+    } else {
+      //Set menu for tablet/desktop screensizes.
+      headerMenu.style.display = 'block'
+      headerMenuLink.style.display = 'none';
+    }
+  }
 })
